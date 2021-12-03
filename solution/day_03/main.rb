f2bd48bb14e08c7b89ccfbc969ab862f74593a7d
David Falusi <falusi.david94@gmail.com>
2021-12-03 19:06:37 +0100

    Add second part of 3rd day's solution

--- solution/day_03/main.rb
+++ solution/day_03/main.rb
@@ -20,3 +20,29 @@ epsilon = eval "0b#{epsilon_bin}"
 puts 'Part #1'
 puts "gamma: #{gamma}, epsilon: #{epsilon}"
 puts "gamma * epsilon: #{gamma * epsilon}"
+
+def bit_count_at(input, position) = input.transpose.map(&:tally).at(position)
+
+def find_rating(input)
+  (0...input.first.length).inject(input) do |previous_list, i|
+    break previous_list if previous_list.count == 1
+
+    bit_counts  = bit_count_at(previous_list, i)
+    fitting_bit = yield(bit_counts)
+
+    previous_list.select { |bits| bits[i] == fitting_bit }
+  end.first.join
+end
+
+input_list = File.readlines(path).map(&:chomp).map(&:chars)
+oxygen_gen_rating_bin =
+  find_rating(input_list) { |bit_counts| bit_counts['0'] > bit_counts['1'] ? '0' : '1' }
+scrubber_rating_bin   =
+  find_rating(input_list) { |bit_counts| bit_counts['0'] > bit_counts['1'] ? '1' : '0' }
+
+oxygen_gen_rating = eval "0b#{oxygen_gen_rating_bin}"
+scrubber_rating   = eval "0b#{scrubber_rating_bin}"
+
+puts 'Part #2'
+puts "oxygen_gen_rating: #{oxygen_gen_rating}, scrubber_rating: #{scrubber_rating}"
+puts "oxygen_gen_rating * scrubber_rating: #{oxygen_gen_rating * scrubber_rating}"
