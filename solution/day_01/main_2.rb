f28c82b20026e9c69591851a7b9d8606001cfadf
David Falusi <falusi.david94@gmail.com>
2021-12-05 14:44:41 +0100

    Use #each_cons for sliding window

--- solution/day_01/main_2.rb
+++ solution/day_01/main_2.rb
@@ -5,12 +5,7 @@ path = File.join(__dir__, 'input.txt')
 changes = []
 windows = []
 
-File.readlines(path).map(&:to_i).inject([]) do |previous_window, depth|
-  window = (previous_window + [depth]).last(3)
-
-  windows << window if window.length == 3
-  window
-end
+File.readlines(path).map(&:to_i).each_cons(3) { |window| windows << window }
 
 windows.map(&:sum).inject(nil) do |previous_depth, depth|
   next depth if previous_depth.nil?
