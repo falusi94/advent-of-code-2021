feffbc018f55ae146dcb953915f979599c81afcf
David Falusi <falusi.david94@gmail.com>
2021-12-10 12:12:24 +0100

    Add 10th day's solution

---
+++ solution/day_10/main.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+path = File.join(__dir__, 'input.txt')
+
+ERROR_SCORE_MAPPING = {
+  ')' => 3,
+  ']' => 57,
+  '}' => 1_197,
+  '>' => 25_137
+}
+
+AUTOCOMPLETE_SCORE_MAPPING = {
+  ')' => 1,
+  ']' => 2,
+  '}' => 3,
+  '>' => 4
+}
+
+def process(line)
+  line.chars.inject([]) do |stack, char|
+    case [char, stack.last]
+    in ['(', *] then stack << ')'
+    in ['[', *] then stack << ']'
+    in ['{', *] then stack << '}'
+    in ['<', *] then stack << '>'
+    in [current, expected] if current == expected then stack[..-2]
+    else return char
+    end
+  end
+end
+
+corrupted_lines, incomplete_lines =
+  File
+  .readlines(path)
+  .map { |line| process(line.chomp) }
+  .partition { |result| result.is_a?(String) }
+
+error_score = corrupted_lines.map { |incorrect_char| ERROR_SCORE_MAPPING[incorrect_char] }.sum
+
+puts "The eror score is #{error_score}"
+
+autocomplete_scores =
+  incomplete_lines
+  .map { |missing_chars| missing_chars.reverse.map { |char| AUTOCOMPLETE_SCORE_MAPPING[char] } }
+  .map { |scores| scores.inject(0) { |total, score| total * 5 + score } }
+  .sort
+
+autocomplete_score = autocomplete_scores[(autocomplete_scores.count - 1) / 2]
+
+puts "The autocomplete score is #{autocomplete_score}"
